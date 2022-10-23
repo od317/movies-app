9e520c6a4c13f888dc3eb87b3fcbd06f0a964244
osama drbas <osamadrbas31@gmail.com>
2022-10-23 18:36:40 +0300

    new

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require('express');
 const app = express();
 const expresslay  = require('express-ejs-layouts');
 const method = require('method-override');
+const PORT = process.env.PORT || 3000;
 
-
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -23,4 +18,4 @@ app.get('/',(req,res)=>{
 })
 
 
-app.listen(process.env.port||3000,console.log('server is runnign'));
+app.listen(PORT);
